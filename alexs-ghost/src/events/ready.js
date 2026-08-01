7971ca36cb8cdbe06429153bf7a40e1d661c7b1f
StNnoji <nnoji2682@gmail.com>
2026-08-01 13:31:45 +0500

    Disable proactive Ghost DMs

--- alexs-ghost/src/events/ready.js
+++ alexs-ghost/src/events/ready.js
@@ -1,20 +1,14 @@
 const { Events, ActivityType } = require("discord.js");
-const { startCheckInService } = require("../services/checkInService");
-const { startDailyRoutineService } = require("../services/dailyRoutineService");
 const logger = require("../utils/logger");
-const { sendOneTimeCampaigns } = require("../services/oneTimeCampaignService");
 
 module.exports = {
   name: Events.ClientReady,
   once: true,
-  async execute(client) {
+  execute(client) {
     logger.info(`Logged in as ${client.user.tag}`);
     client.user.setPresence({
       activities: [{ name: "over tiny ghost smiles 👻🌸", type: ActivityType.Watching }],
       status: "online"
     });
-    await sendOneTimeCampaigns(client);
-    startCheckInService(client);
-    startDailyRoutineService(client);
   }
 };
